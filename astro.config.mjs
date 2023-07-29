5bb663323524cbe608810a068716170b92fb8ac0
krishna <kp064342@gmail.com>
2023-07-29 22:09:00 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 
+
 // https://astro.build/config
 export default defineConfig({
 
   site: 'https://krishna-blog-clip.netlify.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap()],
+  
 });
